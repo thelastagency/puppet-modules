4020b812640ca117d84afcc46adc7236b39a5590
lostboy <paul.crabtree@gmail.com>
2010-12-06 10:44:08 +0530

    fixed common rpm path

--- mcollective/manifests/common.pp
+++ mcollective/manifests/common.pp
@@ -2,6 +2,6 @@ class mcollective::common {
 	package { "mcollective-common": 
        provider => rpm, 
        ensure => installed, 
-       source => "http://puppetlabs.com/downloads/mcollective/mcollective-0.4.10-1.el5.noarch.rpm",
+       source => "http://puppetlabs.com/downloads/mcollective/mcollective-common-0.4.10-1.el5.noarch.rpm",
 	}
 }
